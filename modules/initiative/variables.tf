c5f234e0be17b1389ecc283773d524a46308c205
Jan Szczyra <jan.szczyra@gmail.com>
2022-09-15 14:10:52 +0200

    do it on definition_builtin module

--- modules/initiative/variables.tf
+++ modules/initiative/variables.tf
@@ -74,7 +74,7 @@ locals {
   # colate all definition parameters into a single object
   member_parameters = {
     for d in var.member_definitions :
-    d.name => try(jsondecode(d.parameters), {})
+    d.name => try(jsondecode(d.parameters), null)
   }
 
   # combine all discovered definition parameters using interpolation
