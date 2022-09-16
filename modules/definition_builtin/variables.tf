e18ce0d212b88f25c04fc68fc6a2cd4843e1f14c
Jan Szczyra <jan.szczyra@gmail.com>
2022-09-16 13:23:41 +0200

    no 64 chars limit though

--- modules/definition_builtin/variables.tf
+++ modules/definition_builtin/variables.tf
@@ -9,10 +9,10 @@ variable "policy_name" {
   description = "Name to be used for this policy, when using the module library this should correspond to the correct category folder under /policies/policy_category/policy_name. Changing this forces a new resource to be created."
   default     = ""
 
-  validation {
-    condition     = length(var.policy_name) <= 64
-    error_message = "Definition names have a maximum 64 character limit, ensure this matches the filename within the local policies library."
-  }
+  # validation {
+  #   condition     = length(var.policy_name) <= 64
+  #   error_message = "Definition names have a maximum 64 character limit, ensure this matches the filename within the local policies library."
+  # }
 }
 
 # variable display_name {
